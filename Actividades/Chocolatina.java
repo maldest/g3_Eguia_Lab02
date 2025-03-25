ad47f35491c26e2e075965c3fdbf765a7ad6b4ae
maldest <andre.eguia01@gmail.com>
2025-03-25 16:17:13 -0500

    Create Chocolatina.java
    creacopm de chocolatina

---
+++ Actividades/Chocolatina.java
@@ -0,0 +1,15 @@
+package g3_Eguia_Lab02.Actividades;
+
+public class Chocolatina {
+    private String marca;
+
+    public Chocolatina(String marca){
+        this.marca= marca;
+    }
+    public String getMarca(){
+        return marca;
+    }
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+}
